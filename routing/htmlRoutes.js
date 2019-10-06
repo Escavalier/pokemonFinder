9626d4285be5ff640a71eb96f020d3181432bc7f
juliagraves <gravehaus@gmail.com>
2019-10-06 13:36:48 -0700

    Rewrote folder to reflect submitted app

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -1,13 +1,40 @@
-var path = require("path");
+//_____      _                                ______    _                _   ______ _           _
+//|  __ \    | |                              |  ____|  (_)              | | |  ____(_)         | |
+//| |__) |__ | | _____ _ __ ___   ___  _ __   | |__ _ __ _  ___ _ __   __| | | |__   _ _ __   __| | ___ _ __
+//|  ___/ _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \  |  __| '__| |/ _ \ '_ \ / _` | |  __| | | '_ \ / _` |/ _ \ '__|
+//| |  | (_) |   <  __/ | | | | | (_) | | | | | |  | |  | |  __/ | | | (_| | | |    | | | | | (_| |  __/ |
+//|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_| |_|  |_|  |_|\___|_| |_|\__,_| |_|    |_|_| |_|\__,_|\___|_|
+//
 
-module.exports = function(app) {
-  // if user enters survey in URL or presses survey button, serves the survey HTML file
-  app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+
+
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+let path = require("path");
+console.log(require)
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get("./survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/survey.html")); //change '__dirname' to survey?//
   });
 
-  // fallback use route for homepage
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  console.log(app)
+
+
+  // If no matching route is found default to home
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./public/home.html"));
   });
 };
