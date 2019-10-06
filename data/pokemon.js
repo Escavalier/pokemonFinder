07accf675e60069f3830da191a69bf7e246db299
juliagraves <gravehaus@gmail.com>
2019-10-05 19:17:53 -0700

    added styling

---
+++ data/pokemon.js
@@ -0,0 +1,87 @@
+
+
+
+//_____      _                                ______    _                _   ______ _           _
+//|  __ \    | |                              |  ____|  (_)              | | |  ____(_)         | |
+//| |__) |__ | | _____ _ __ ___   ___  _ __   | |__ _ __ _  ___ _ __   __| | | |__   _ _ __   __| | ___ _ __
+//|  ___/ _ \| |/ / _ \ '_ ` _ \ / _ \| '_ \  |  __| '__| |/ _ \ '_ \ / _` | |  __| | | '_ \ / _` |/ _ \ '__|
+//| |  | (_) |   <  __/ | | | | | (_) | | | | | |  | |  | |  __/ | | | (_| | | |    | | | | | (_| |  __/ |
+//|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_| |_|  |_|  |_|\___|_| |_|\__,_| |_|    |_|_| |_|\__,_|\___|_|
+//
+
+
+
+
+
+// https://bulbapedia.bulbagarden.net/wiki/Category:New_Pok%C3%A3mon
+
+//keep alphabetized
+
+//I used a random number-string generator so technically these 'stats' are likely canonically incorrect. Lame.
+
+const pokemon = [
+  {
+    name: "Alcremie",
+    photo:"https://cdn.bulbagarden.net/upload/4/4d/Alcremie.png", //insert image here
+    scores: [2,2,4,5,2,5,2,3,2,2] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Corviknight",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/2/2e/Corviknight.png/1200px-Corviknight.png", //insert image here
+    scores: [3,2,4,5,2,3,3,2,1,3] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Cramorant",
+    photo:"https://cdn.bulbagarden.net/upload/3/31/Cramorant.png", //insert image here
+    scores: [3,5,5,5,5,2,2,1,2,4] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Drednaw",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/4/4e/Drednaw.png/1200px-Drednaw.png", //insert image here
+    scores: [2, 5, 4, 3, 1, 3, 2, 5, 3,1] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Duraludon",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/2/22/Duraludon.png/1200px-Duraludon.png", //insert image here
+    scores: [2, 5, 1, 2, 3, 4, 5, 3, 3,3] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Gossifleur",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/2/23/Gossifleur.png/1200px-Gossifleur.png", //insert image here
+    scores: [2, 4, 2, 3, 3, 5, 5, 3, 1,3] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Sirfetch'd",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/0/0e/Sirfetch%23d.png/1200px-Sirfetch%23d.png", //insert image here
+    scores: [3, 5, 2, 3, 5, 2, 4, 1, 3,5] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Sobble",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/3/3b/215Sobble.png/1200px-215Sobble.png", //insert image here
+    scores: [2, 3, 5, 2, 5, 3, 1, 4, 3,2] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Wooloo",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/2/2f/Wooloo.png/1200px-Wooloo.png", //insert image here
+    scores: [4, 2, 1, 2, 3, 5, 5, 3, 3,3] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+  {
+    name: "Zamazenta",
+    photo:"https://cdn.bulbagarden.net/upload/thumb/b/bd/Zamazenta.png/1200px-Zamazenta.png", //insert image here
+    scores: [3, 3, 5, 3, 5, 1, 2, 2, 4,5] //hp, attack, defense, spec def, spec attack, speed, agility, evolution, kindness, happiness
+  },
+
+
+
+];
+// export the array
+module.exports = pokemon;
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,17 +5,3 @@
 //| |  | (_) |   <  __/ | | | | | (_) | | | | | |  | |  | |  __/ | | | (_| | | |    | | | | | (_| |  __/ |
 //|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_| |_|  |_|  |_|\___|_| |_|\__,_| |_|    |_|_| |_|\__,_|\___|_|
 //
-
-// server.js
-
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(process.env.PORT || 4000, function(){
-  console.log('Your node js server is running');
-});
