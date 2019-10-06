dab4fff4913c612f228b391e4fe022eb0829343d
juliagraves <gravehaus@gmail.com>
2019-10-05 19:44:46 -0700

    added routes

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -5,3 +5,29 @@
 //| |  | (_) |   <  __/ | | | | | (_) | | | | | |  | |  | |  __/ | | | (_| | | |    | | | | | (_| |  __/ |
 //|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_| |_|  |_|  |_|\___|_| |_|\__,_| |_|    |_|_| |_|\__,_|\___|_|
 //
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require('path');
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  });
+
+  // If no matching route is found default to home
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+  });
+};
--- server.js
+++ server.js
@@ -6,16 +6,82 @@
 //|_|   \___/|_|\_\___|_| |_| |_|\___/|_| |_| |_|  |_|  |_|\___|_| |_|\__,_| |_|    |_|_| |_|\__,_|\___|_|
 //
 
-// server.js
 
-const express = require('express');
-const app = express();
-const path = require('path');
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+
+
+
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use('/static', express.static(path.join(__dirname, 'app/public')))
+
+
+
+
+
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+
+
+
+
+
+
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
 
-app.listen(process.env.PORT || 4000, function(){
-  console.log('Your node js server is running');
-});
+// // server.js
+//
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+//
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'home.html'));
+// });
+//
+// app.listen(process.env.PORT || 4000, function(){
+//   console.log('Your node js server is running');
+// });
